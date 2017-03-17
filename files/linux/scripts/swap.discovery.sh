44c7a0bfdce255335bae5328db546c285a5c8a59
Mamedaliev Kirill <danteg41@gmail.com>
2017-03-17 18:56:32 +0500

    swap.discovery: fix json, when swap turn off

--- files/linux/scripts/swap.discovery.sh
+++ files/linux/scripts/swap.discovery.sh
@@ -4,14 +4,14 @@
 
 SIZE=$(swapon -s |grep -v ^Filename |awk '{sum += $3} END {print sum}')
 
-if [ -z $SIZE ]; then exit 1; fi
-
 printf "{\n";
 printf "\t\"data\":[\n\n";
 
-printf "\t{\n";
-printf "\t\t\"{#SWAP_EXISTS}\":\"$line\"\n";
-printf "\t}\n";
+if [ ! -z $SIZE ]; then
+  printf "\t{\n";
+  printf "\t\t\"{#SWAP_EXISTS}\":\"\"\n";
+  printf "\t}\n";
+fi
 
 printf "\n\t]\n";
 printf "}\n";
