81532ea45f63b678826ecea67d974e93384e9657
Mamedaliev Kirill <danteg41@gmail.com>
2020-10-12 18:17:36 +0500

    perf(pgsql.connections): PG10+ connections metrics

--- files/postgresql/scripts/pgsql.connections.sh
+++ files/postgresql/scripts/pgsql.connections.sh
@@ -15,29 +15,76 @@ if [ "$#" -lt 2 ];
 fi
 PARAM="$1"
 
-case "$PARAM" in
-'idle_in_transaction' )
-        query="SELECT COUNT(*) FROM pg_stat_activity WHERE state = 'idle in transaction';"
+read_cached_pgver() {
+  PG_VER=$(<${1})
+}
+update_cached_pgver() {
+  psql -qAtX -F: -c "SHOW server_version" -h localhost -U "$username" "$dbname"|grep -oP "^(9\.[0-9]+|[0-9]+)" > ${1}
+}
+
+# Кэширование версии PG
+CACHE_TIME=$(date -d 'now - 1hour' +%s)
+TMP_FILE=/tmp/zabbix_${dbname}_pgver.tmp
+if [ -f ${TMP_FILE} ]; then
+  TMP_TIME=$(stat -c%Y ${TMP_FILE})
+  if (( ${TMP_TIME} <= ${CACHE_TIME} )); then
+    update_cached_pgver ${TMP_FILE}
+  fi
+  read_cached_pgver ${TMP_FILE}
+else
+  update_cached_pgver ${TMP_FILE}
+  read_cached_pgver ${TMP_FILE}
+fi
+
+case "$PG_VER" in
+9.[4-6] )
+  case "$PARAM" in
+  'idle_in_transaction' )
+          query="SELECT COUNT(*) FROM pg_stat_activity WHERE state IN ('idle in transaction', 'idle in transaction (aborted)');"
+  ;;
+  'idle' )
+          query="SELECT COUNT(*) FROM pg_stat_activity WHERE state = 'idle';"
+  ;;
+  'total' )
+          query="SELECT COUNT(*) FROM pg_stat_activity;"
+  ;;
+  'active' )
+          query="SELECT COUNT(*) FROM pg_stat_activity WHERE state = 'active';"
+  ;;
+  'waiting' )
+          query="SELECT COUNT(*) FROM pg_stat_activity WHERE waiting <> 'f';"
+  ;;
+  'waiting_event' )
+          query="SELECT COUNT(*) FROM pg_stat_activity WHERE wait_event IS NOT NULL;"
+  ;;
+  'total_pct' )
+          query="select count(*)*100/(select (setting::int) from pg_settings where name = 'max_connections') from pg_stat_activity;"
+  ;;
+  * ) echo "ZBX_NOTSUPPORTED"; exit 1;;
+  esac
 ;;
-'idle' )
-        query="SELECT COUNT(*) FROM pg_stat_activity WHERE state = 'idle';"
-;;
-'total' )
-        query="SELECT COUNT(*) FROM pg_stat_activity;"
-;;
-'active' )
-        query="SELECT COUNT(*) FROM pg_stat_activity WHERE state = 'active';"
-;;
-'waiting' )
-        query="SELECT COUNT(*) FROM pg_stat_activity WHERE waiting <> 'f';"
-;;
-'waiting_event' )
-        query="SELECT COUNT(*) FROM pg_stat_activity WHERE wait_event IS NOT NULL;"
-;;
-'total_pct' )
-        query="select count(*)*100/(select (setting::int) from pg_settings where name = 'max_connections') from pg_stat_activity;"
-;;
-* ) echo "ZBX_NOTSUPPORTED"; exit 1;;
+* )
+  case "$PARAM" in
+  'idle_in_transaction' )
+          query="SELECT COUNT(*) FROM pg_stat_activity WHERE state IN ('idle in transaction', 'idle in transaction (aborted)') AND backend_type = 'client backend';"
+  ;;
+  'idle' )
+          query="SELECT COUNT(*) FROM pg_stat_activity WHERE state = 'idle' AND backend_type = 'client backend';"
+  ;;
+  'total' )
+          query="SELECT COUNT(*) FROM pg_stat_activity WHERE state IS NOT NULL AND backend_type = 'client backend';"
+  ;;
+  'active' )
+          query="SELECT COUNT(*) FROM pg_stat_activity WHERE state = 'active' AND backend_type = 'client backend';"
+  ;;
+  'waiting_event' )
+          query="SELECT COUNT(*) FROM pg_stat_activity WHERE wait_event IS NOT NULL AND backend_type = 'client backend';"
+  ;;
+  'total_pct' )
+          query="select count(*)*100/(select (setting::int) from pg_settings where name = 'max_connections') from pg_stat_activity WHERE state IS NOT NULL AND backend_type = 'client backend';"
+  ;;
+  * ) echo "ZBX_NOTSUPPORTED"; exit 1;;
+  esac
 esac
 
 psql -qAtX -F: -c "$query" -h localhost -U "$username" "$dbname"
